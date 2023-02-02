8bda4e80374d474f9ca7f67580fbc5080ef59642
Illia <ichorny.ilya25@gmail.com>
2023-02-02 15:04:58 +0100

    change const

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 console.log('oleg')
 document.body.innerHTML = 'hallo'
 console.log('second oleg')
-const third = 'third commit'
-console.log(third[5])
+const third = 'third commit!!!!!!!!!!!'
+console.log(third[7])
